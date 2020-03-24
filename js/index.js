207fd20acdc4163d27612dab1811581d08e939b2
devdevil-production <saudd2gh@gmail.com>
2020-03-24 13:25:31 +0400

    Add createNav method

--- js/index.js
+++ js/index.js
@@ -10,7 +10,29 @@ class DD_Carousel {
     this.slideSize = this.slides[0].getBoundingClientRect();
     this.slideWidth = this.slideSize.width;
 
-    this.nextButton = $(".dd-carousel_button.right-button");
-    this.prevButton = $(".dd-carousel_button.left-button");
+    this.nextButton = $(".dd-carousel_button.next-button");
+    this.prevButton = $(".dd-carousel_button.prev-button");
+
+    this.createNav();
+
+    // this.carouselNav = $(".dd-carousel_nav");
+    // this.indicators = Array.from(
+    //   this.carouselNav.children(".dd-carousel_indicator")
+    // );
+  }
+
+  createNav() {
+    this.carouselNav = $(`<div class="dd-carousel_nav"></div>`);
+    this.indicators = this.slides.map((_, i) =>
+      $(
+        `<button class="dd-carousel_indicator${
+          i === 0 ? " active" : ""
+        }"></button>`
+      )
+    );
+
+    this.indicators.forEach(indicator => this.carouselNav.append(indicator));
+
+    this.nextButton.parent().append(this.carouselNav);
   }
 }
